e5285a717a1d90fce9d660f78e3ee0d3ca8a363d
carlosguttemberg <guttembergcarlos@gmail.com>
2020-02-23 14:27:00 -0300

    Listando os clientes usando o jquery

--- resources/views/indexjs.blade.php
+++ resources/views/indexjs.blade.php
@@ -23,7 +23,7 @@
 
                 <div class="card-body">
                     <h5 class="card-title"></h5>
-                    <table class="table table-hover">
+                    <table class="table table-hover" id="tabelaClientes">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
@@ -45,5 +45,38 @@
         </div>        
 
         <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+
+        <script type="text/javascript">
+
+            function montarLinha(cliente) {
+                return  "<tr>" + 
+                            "<td>" + cliente.id         + "</td>" +
+                            "<td>" + cliente.nome       + "</td>" +
+                            "<td>" + cliente.sobrenome  + "</td>" +
+                            "<td>" + cliente.email      + "</td>" +
+                        "<tr>";
+            }
+
+            function montarTabela(data){
+                $("#tabelaClientes>tbody>tr").remove();
+
+                for (i = 0; i < data.data.length; i++) {
+                    s = montarLinha(data.data[i]);
+
+                    $("#tabelaClientes tbody").append(s);
+                }
+            }
+
+            function carregarClientes(pagina) {
+                $.get('/json', {page: pagina}, function (resp) {
+                    montarTabela(resp);
+                });
+            }
+            
+
+            $(function(){
+                carregarClientes(1);
+            });
+        </script>
     </body>
 </html>
